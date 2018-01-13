a7bf41f085f988f830cda17ad529cab8d5435418
JoseInside <josemonr@ucm.es>
2018-01-13 22:33:00 +0100

    Añadidos archivos de material necesarios para empezar

---
+++ media/App/vertexShaderE2_.glsl
@@ -0,0 +1,10 @@
+vertex_program vertexShaderE2 {
+	source vertexShaderE2glsl.txt; // archivo
+
+	default_params //valores de las variables uniform
+	{
+	param_named_auto modelViewProjMat; worldviewproj_matrix
+	
+	}
+}
+
